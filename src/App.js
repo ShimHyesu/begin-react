cd930a886af26a03b7edcbf159ff8d16f41d8d0a
Hyesu <hesushim@gmail.com>
2022-06-22 06:46:37 +0900

    basic_JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Hello from './Hello.js';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, //기본 단위 픽셀
+    padding: '1rem',
+  }
   return (
-    <div>
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
