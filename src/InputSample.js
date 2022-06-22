2d7b7d2d253daf46af9ce1deed442120fa1e6f8e
Hyesu <hesushim@gmail.com>
2022-06-22 23:45:34 +0900

    basic_useRef_특정 DOM 선택하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: '',
     });
+
+    //함수형 컴포넌트에서 ref객체 사용할 때 useRef라는 Hook함수 사용
+    const nameInput = useRef();
 
     //비구조화 할당을 통해 값 추출
     const {name, nickname} = inputs;
@@ -20,12 +23,15 @@ function InputSample() {
         setInputs({
             name:'',
             nickname:'',
-        })
+        });
+        //Ref객체의 .current값은 우리가 원하는 DOM 가리키게 됨
+        nameInput.current.focus();
     };
 
     return(
         <div>
-            <input name='name' placeholder='이름' onChange={onChange} value={name} />
+            {/*ref객체를 우리가 선택하고 싶은 DOM에 ref값으로 설정*/}
+            <input name='name' placeholder='이름' onChange={onChange} value={name} ref={nameInput}/>
             <input name='nickname' placeholder='닉네임' onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
