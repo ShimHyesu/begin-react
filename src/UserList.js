847c666d85d2112b26d81080e6f4f47d3a3bd775
Hyesu <hesushim@gmail.com>
2022-06-23 23:13:36 +0900

    basic_useEffect

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({user, onRemove, onToggle}) {
+    //마운트/언마운트 관리
+    // useEffect(()=> {
+    //     console.log('컴포넌트 화면에 나타남');
+    //     return () => {
+    //         console.log('컴포넌트 화면에서 사라짐');
+    //     };
+    // },[]);
+
+    //deps에 특정 값 넣기
+    // useEffect(() => {
+    //     console.log('user값이 설정됨');
+    //     console.log(user);
+    //     return () =>{
+    //         console.log('user가 바뀌기 전..');
+    //         console.log(user);
+    //     };
+    // },[user]);
+    useEffect(()=>{
+        console.log(user);
+    });
+
     return(
         <div>
             <b
