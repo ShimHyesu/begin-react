f2eefefa8ec26d3bb459e2747ecb0759dec98ea1
Hyesu <hesushim@gmail.com>
2022-06-25 18:06:14 +0900

    basic_useInputs를 useReducer로 구현

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -1,14 +1,42 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useReducer} from 'react';
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE_INPUT':
+            return {
+                ...state,
+                [action.name]:action.value,
+            }
+        case 'RESET':
+            return Object.keys(state).reduce((acc,current) =>{
+                acc[current] = '';
+                return acc;
+            },{});
+        default: 
+            return state;
+    }
+}
+
+
+//useState로 구현한 useInputs를 useReducer을 사용해 구현
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
 
     const onChange = useCallback(e =>{
         const {name, value} = e.target;
-        setForm(form => ({...form, [name]:value}));
+        dispatch({
+            type: 'CHANGE_INPUT',
+            name,
+            value,
+        })
     },[]);
 
-    const reset = useCallback(()=> setForm(initialForm), [initialForm]);
+    const reset = useCallback(()=>
+        dispatch({
+            type:'RESET',
+        })
+    ,[]);
+
     return [form, onChange, reset];
 }
 
