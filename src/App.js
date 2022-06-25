df85ade2fe84e6f1382945e26ed12bc882b5579f
Hyesu <hesushim@gmail.com>
2022-06-25 16:56:46 +0900

    basic_커스텀Hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useMemo, useReducer, useRef } from 'react';
 
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 import UserList from './UserList';
 
 //활성 사용자 수 구하는 함수
@@ -10,10 +11,6 @@ function countActiveUsers(users) {
 }
 
 const initialState = {
-  inputs: {
-    username: '',
-    email: '',
-  },
   users:[
     {
       id:1,
@@ -36,33 +33,19 @@ const initialState = {
   ]
 };
 
-//reducer: 현재 상태와 액션 객체를 파라미터로 받아와서 새로운 상태로 반환
-//→ 컴포넌트가 지닐 새로운 상태
-//action객체: 업데이트를 위한 정보
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT': 
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]:action.value
-        }
-      };
     case 'CREATE_USER':
       return {
-        inputs:initialState.inputs,
         users: state.users.concat(action.user),
       };
     case 'TOGGLE_USER':
       return {
-        ...state,
         users:state.users.map(user => 
           user.id===action.id?{...user, active:!user.active}:user)
       };
     case 'REMOVE_USER' :
       return {
-        ...state,
         users:state.users.filter(user => user.id!==action.id)
       };
     default:
@@ -72,26 +55,21 @@ function reducer(state, action) {
 }
 
 function App() {
-  //useReducer: 컴포넌트의 상태 업데이트 로직을 컴포넌트에서 분리
-  //state: 컴포넌트에서 사용할 수 있는 상태
-  //dispatch: 액션을 발생시키는 함수
+
+  //useInputs라는 커스텀 Hook 사용
+  const [{username, email}, onChange, reset] = useInputs({
+    username:'',
+    email:'',
+  });
+
   const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
   const {users} = state;
-  const {username, email} = state.inputs;
+
+
 
   const count = useMemo(()=>countActiveUsers(users),[users]);
-
-  //input값 변경
-  const onChange = useCallback(e => {
-    const {name, value} = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value,
-    });
-  },[]);
 
   //사용자 등록
   const onCreate = useCallback(() => {
@@ -103,6 +81,7 @@ function App() {
         email,
       }
     });
+    reset();
     nextId.current+=1;
   },[username, email]);
 
